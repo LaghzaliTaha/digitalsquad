3fbbe82899468e98eba8b35b049098c57ba49c22
LAGHZALI taha <mrtahalz@gmail.com>
2021-10-09 12:48:22 +0100

    update message and status

--- mailing.js
+++ mailing.js
@@ -5,14 +5,12 @@ document.getElementById("getintouch").addEventListener("submit", function(e) {
     var xhr=new XMLHttpRequest();
     xhr.onreadystatechange=function()
     {
-      if (this.readyState == 4 && this.status == 200)
+      if (this.readyState == 4 && this.status == 201)
       {
-
-
             var res=this.response;
              if(res.message)
              {    swal({
-                title: "Good job!",
+                title: "Mail sent successfully",
                 text: res.msg,
                 icon: "success",
               });
